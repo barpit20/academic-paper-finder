135bccaaae6769d2da2b76eddfa5a9ddab970887
Tor-Salve <mhb558@alumni.ku.dk>
2020-08-06 15:19:31 +0200

    changed pre- and postprocessing to more flexibility;

--- finder.py
+++ finder.py
@@ -33,6 +33,11 @@ class Fetcher:
         self.paper_is_json = self.get_config('urls.paper.expect-json', False)
         self.per_page = self.get_config('urls.list.per-page')
 
+        self.preprocessing_paper = self.get_config("preprocessing.paper", []) + kwargs.get('preprocessing_paper', [])
+        self.preprocessing_list = self.get_config("preprocessing.list", []) + kwargs.get('preprocessing_list', [])
+        self.postprocessing_paper = self.get_config("postprocessing.paper", []) + kwargs.get('postprocessing_paper', [])
+        self.postprocessing_list = self.get_config("postprocessing.list", []) + kwargs.get('postprocessing_list', [])
+
         self.restrict_identifiers_to = kwargs.get('restrict_identifiers_to', [])
 
     @property
@@ -46,9 +51,11 @@ class Fetcher:
         safe_identifier = h.safe_filename(identifier)
         return "%s/%s/papers/%s.html" % (self.cache_folder, self.config_name, safe_identifier)
     
-    def pdf_file_name(self, identifier):
+    def pdf_file_name(self, identifier, only_filename=False):
         safe_identifier = h.safe_filename(identifier)
-        return "%s/%s/pdfs/%s.pdf" % (self.cache_folder, self.config_name, safe_identifier)
+        filename = "%s.pdf" % safe_identifier
+        if only_filename: return filename
+        return "%s/%s/pdfs/%s" % (self.cache_folder, self.config_name, filename)
 
     def result_file_name(self):
         return "%s/%s/result_%s.json" % (self.cache_folder, self.config_name, self.name)
@@ -251,31 +258,32 @@ class Fetcher:
     def doi(self, _paper):
         return self.from_paper("doi", _paper)
 
+    def doi_url(self, _paper):
+        return "https://doi.org/%s" % self.doi(_paper)
+
     def preprocess_paper(self, _paper):
-        preprocessing = self.get_config("preprocessing.paper", [])
-        if len(preprocessing) > 0:
-            for pre in preprocessing:
+        if len(self.preprocessing_paper) > 0:
+            for pre in self.preprocessing_paper:
                 if pre["type"] == "embedded_json":
                     raw_paper = self.re_item(pre['regex'], _paper)
                     _paper = json.loads(raw_paper)
         return _paper
     
     def preprocess_list(self, _list):
-        preprocessing = self.get_config("preprocessing.list", [])
-        if len(preprocessing) > 0:
-            for _ in preprocessing:
+        if len(self.preprocessing_list) > 0:
+            for _, _ in enumerate(self.preprocessing_list):
                 pass
         return _list
 
     def postprocess_paper(self, paper):
-        postprocessing = self.get_config("postprocessing.paper", [])
-        if len(postprocessing) > 0:
-            for i, post in enumerate(postprocessing):
+        if len(self.postprocessing_paper) > 0:
+            for i, post in enumerate(self.postprocessing_paper):
                 if post["type"] == "query":
-                    f = h.analyze_query(paper, self.get_config(f"postprocessing.paper.{i}.query", {}))
+                    query = h.get_dict_field(self.postprocessing_paper, f"{i}.query", {})
+                    f = h.analyze_query(paper, query)
                     paper = paper if f else None
         return paper
-
+    
     def parse_paper(self, identifier, _paper):
         _paper = self.preprocess_paper(_paper)
         paper = {
@@ -288,8 +296,9 @@ class Fetcher:
             'citations': self.citations(_paper),
             'isbn': self.isbn(_paper),
             'doi': self.doi(_paper),
-
+            'doi_url': self.doi_url(_paper),
             'pdf_url': self.pdf_url(identifier),
+            'pdf_file_name': self.pdf_file_name(identifier, True),
             'paper_url': self.paper_url(identifier),
             'identifier': identifier
         }
@@ -312,6 +321,7 @@ class Fetcher:
 
     def fetch_parse_list(self, page):
         _list = self.fetch_list(page)
+        _list = self.preprocess_list(_list)
         identifiers = self.identifiers(_list)
         return _list, self.fetch_parse_papers(identifiers)
 
@@ -330,7 +340,7 @@ class Fetcher:
         pages_to_fetch = math.ceil(result['total_results'] / self.per_page) - 1
 
         sleep_for = self.get_config('sleep_between_requests')
-        print("This will take at least %.2f hours if the cache is clean" % ((result['total_results'] * sleep_for) / 60 / 60))
+        print("This will take at least %.2f minutes if the cache is clean" % ((result['total_results'] * sleep_for) / 60))
 
         if ((pages_to_fetch) > 0):
             print(" - fetching the rest of the pages: %d" % (pages_to_fetch))
@@ -438,56 +448,56 @@ class Fetcher:
 if __name__ == "__main__":
 
     finders = [
-        # UIST
-        {
-            "name": "uist",
-            "config_name": "acm",
-            "search_parameters": {
-                'fillQuickSearch': 'false',
-                'ContentItemType': 'research-article',
-                'SpecifiedLevelConceptID': '119271',
-                'expand': 'dl',
-                'AfterMonth': '1',
-                'AfterYear': '2000',
-                'BeforeMonth': '12',
-                'BeforeYear': '2019',
-                'AllField': 'Title:((select* OR manipulat*) AND ("virtual" OR "VR")) OR Abstract:((select* OR manipulat*) AND ("virtual" OR "VR"))'
-            }
-        },
-        # CHI
-        {
-            "name": "chi",
-            "config_name": "acm",
-            "search_parameters": {
-                'fillQuickSearch': 'false',
-                'ContentItemType': 'research-article',
-                'SpecifiedLevelConceptID': '119596',
-                'expand': 'dl',
-                'AfterMonth': '1',
-                'AfterYear': '2000',
-                'BeforeMonth': '12',
-                'BeforeYear': '2019',
-                'AllField': 'Title:((select* OR manipulat*) AND ("virtual" OR "VR")) OR Abstract:((select* OR manipulat*) AND ("virtual" OR "VR"))'
-            }
-        },
-        # IEEEVR
-        {
-            "name": "ieeevr",
-            "config_name": "ieee",
-            "search_parameters": {
-                "action":"search",
-                "matchBoolean":'True',
-                "newsearch":'True',
-                "queryText":"((((\"Document Title\": \"select*\" OR \"Document Title\": \"manipulat*\") AND (\"Document Title\": \"virtual\" OR \"Document Title\": \"VR\")) OR ((\"Abstract\": \"select*\" OR \"Abstract\": \"manipulat*\") AND (\"Abstract\": \"virtual\" OR \"Abstract\": \"VR\"))) AND (\"Publication Number\": 8787730 OR \"Publication Number\": 8423729 OR \"Publication Number\": 7889401 OR \"Publication Number\": 7499993 OR \"Publication Number\": 7167753 OR \"Publication Number\": 6786176 OR \"Publication Number\": 6542301 OR \"Publication Number\": 6179238 OR \"Publication Number\": 5753662 OR \"Publication Number\": 5440859 OR \"Publication Number\": 4806856 OR \"Publication Number\": 4472735 OR \"Publication Number\": 4160976 OR \"Publication Number\": 11055 OR \"Publication Number\": 9989 OR \"Publication Number\": 9163 OR \"Publication Number\": 8471 OR \"Publication Number\": 7826 OR \"Publication Number\": 7269 OR \"Publication Number\": 6781))",
-                "highlight":'True',
-                "returnFacets":["ALL"],
-                "returnType":"SEARCH",
-                "matchPubs":'True'
-            },
-            'headers': {
-                'Cookie': "TS01b03060_26=014082121d70df1295452f0415c1d19d6df9a6f4e38bd41c2416ed80d5811061c6870f476e50edbc299092405b397624fa7a9e687bc99f8ff8a3910f51ad7f146893751e06; s_sq=%5B%5BB%5D%5D; WT_FPC=id=e72f05a8-a59d-4ec5-aec4-1152a199d9c3:lv=1595292510272:ss=1595292510272; JSESSIONID=GUl75h76VZMOs-zuTCaJlFC4zcJ3yP6wtx-cjO8IeePXFReL5bCx!-680779053; ERIGHTS=qdfpefSlqu3uiCGuTKfJucHgx2BppfxxYF3*xxqgiIyDwkBkIPSB6fG511wx3Dx3D-18x2dsWA2HgFqGnNTPTpUU0DGKQx3Dx3DdYHuM4AonsaMwzx2BLPW6f1Ax3Dx3D-seCuihlon4Yn6ravae9KzQx3Dx3D-x2BoJmAJK1Z3DbmTWzFvGVAAx3Dx3D; utag_main=v_id:01735d32e2490000e5080caf193f03072007c06a00fb8$_sn:7$_ss:0$_st:1595513350925$vapi_domain:ieee.org$_se:6$ses_id:1595511548855%3Bexp-session$_pn:1%3Bexp-session; AMCV_8E929CC25A1FB2B30A495C97%40AdobeOrg=1687686476%7CMCIDTS%7C18464%7CMCMID%7C20641728764065869141128774931607118228%7CMCAAMLH-1596116353%7C6%7CMCAAMB-1596116353%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1595518753s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C3.0.0; xpluserinfo=eyJpc0luc3QiOiJ0cnVlIiwiaW5zdE5hbWUiOiJDb3BlbmhhZ2VuIFVuaXZlcnNpdHkiLCJwcm9kdWN0cyI6IklFTHxNQ1NZTlRINXxNQ1NZTlRIMXxNQ1NZTlRIM3xNQ1NZTlRINHxNQ1NZTlRIMnxNQ1NZTlRIN3xNQ1NZTlRIOHxNQ1NZTlRINnxNQ1NZTlRIOXxNQ1NZTlRIMTB8VkRFfE5PS0lBIEJFTEwgTEFCU3wifQ==; seqId=2644566; WLSESSION=237134476.20480.0000; TS01b03060=012f350623fd5c8dfef53c8016f92209f692fbb878553da77b0d7cb39209593f4e4b723514009901f6c5b52df817feb4a48b460b42d9df924fe4900f2a2d526564dca63f806c55f1e5df6976d59309bdc6da9da559289a09cdd40962dbf4e8de8db87317cf64aab18502ee0ab644b7f10d146a97046589963400ea55e921974f69ce15ca4f1c31e52480121d523b747f9a1b386f836d70ce861539d87205e46153e534c03bdbbad660826925cd8c3991f7bb8ecf77"
-            }
-        },
+        # # UIST
+        # {
+        #     "name": "uist",
+        #     "config_name": "acm",
+        #     "search_parameters": {
+        #         'fillQuickSearch': 'false',
+        #         'ContentItemType': 'research-article',
+        #         'SpecifiedLevelConceptID': '119271',
+        #         'expand': 'dl',
+        #         'AfterMonth': '1',
+        #         'AfterYear': '2000',
+        #         'BeforeMonth': '12',
+        #         'BeforeYear': '2019',
+        #         'AllField': 'Title:((select* OR manipulat*) AND ("virtual" OR "VR")) OR Abstract:((select* OR manipulat*) AND ("virtual" OR "VR"))'
+        #     }
+        # },
+        # # CHI
+        # {
+        #     "name": "chi",
+        #     "config_name": "acm",
+        #     "search_parameters": {
+        #         'fillQuickSearch': 'false',
+        #         'ContentItemType': 'research-article',
+        #         'SpecifiedLevelConceptID': '119596',
+        #         'expand': 'dl',
+        #         'AfterMonth': '1',
+        #         'AfterYear': '2000',
+        #         'BeforeMonth': '12',
+        #         'BeforeYear': '2019',
+        #         'AllField': 'Title:((select* OR manipulat*) AND ("virtual" OR "VR")) OR Abstract:((select* OR manipulat*) AND ("virtual" OR "VR"))'
+        #     }
+        # },
+        # # IEEEVR
+        # {
+        #     "name": "ieeevr",
+        #     "config_name": "ieee",
+        #     "search_parameters": {
+        #         "action":"search",
+        #         "matchBoolean":'True',
+        #         "newsearch":'True',
+        #         "queryText":"((((\"Document Title\": \"select*\" OR \"Document Title\": \"manipulat*\") AND (\"Document Title\": \"virtual\" OR \"Document Title\": \"VR\")) OR ((\"Abstract\": \"select*\" OR \"Abstract\": \"manipulat*\") AND (\"Abstract\": \"virtual\" OR \"Abstract\": \"VR\"))) AND (\"Publication Number\": 8787730 OR \"Publication Number\": 8423729 OR \"Publication Number\": 7889401 OR \"Publication Number\": 7499993 OR \"Publication Number\": 7167753 OR \"Publication Number\": 6786176 OR \"Publication Number\": 6542301 OR \"Publication Number\": 6179238 OR \"Publication Number\": 5753662 OR \"Publication Number\": 5440859 OR \"Publication Number\": 4806856 OR \"Publication Number\": 4472735 OR \"Publication Number\": 4160976 OR \"Publication Number\": 11055 OR \"Publication Number\": 9989 OR \"Publication Number\": 9163 OR \"Publication Number\": 8471 OR \"Publication Number\": 7826 OR \"Publication Number\": 7269 OR \"Publication Number\": 6781))",
+        #         "highlight":'True',
+        #         "returnFacets":["ALL"],
+        #         "returnType":"SEARCH",
+        #         "matchPubs":'True'
+        #     },
+        #     'headers': {
+        #         'Cookie': "TS01b03060_26=014082121d70df1295452f0415c1d19d6df9a6f4e38bd41c2416ed80d5811061c6870f476e50edbc299092405b397624fa7a9e687bc99f8ff8a3910f51ad7f146893751e06; s_sq=%5B%5BB%5D%5D; WT_FPC=id=e72f05a8-a59d-4ec5-aec4-1152a199d9c3:lv=1595292510272:ss=1595292510272; JSESSIONID=GUl75h76VZMOs-zuTCaJlFC4zcJ3yP6wtx-cjO8IeePXFReL5bCx!-680779053; ERIGHTS=qdfpefSlqu3uiCGuTKfJucHgx2BppfxxYF3*xxqgiIyDwkBkIPSB6fG511wx3Dx3D-18x2dsWA2HgFqGnNTPTpUU0DGKQx3Dx3DdYHuM4AonsaMwzx2BLPW6f1Ax3Dx3D-seCuihlon4Yn6ravae9KzQx3Dx3D-x2BoJmAJK1Z3DbmTWzFvGVAAx3Dx3D; utag_main=v_id:01735d32e2490000e5080caf193f03072007c06a00fb8$_sn:7$_ss:0$_st:1595513350925$vapi_domain:ieee.org$_se:6$ses_id:1595511548855%3Bexp-session$_pn:1%3Bexp-session; AMCV_8E929CC25A1FB2B30A495C97%40AdobeOrg=1687686476%7CMCIDTS%7C18464%7CMCMID%7C20641728764065869141128774931607118228%7CMCAAMLH-1596116353%7C6%7CMCAAMB-1596116353%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1595518753s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C3.0.0; xpluserinfo=eyJpc0luc3QiOiJ0cnVlIiwiaW5zdE5hbWUiOiJDb3BlbmhhZ2VuIFVuaXZlcnNpdHkiLCJwcm9kdWN0cyI6IklFTHxNQ1NZTlRINXxNQ1NZTlRIMXxNQ1NZTlRIM3xNQ1NZTlRINHxNQ1NZTlRIMnxNQ1NZTlRIN3xNQ1NZTlRIOHxNQ1NZTlRINnxNQ1NZTlRIOXxNQ1NZTlRIMTB8VkRFfE5PS0lBIEJFTEwgTEFCU3wifQ==; seqId=2644566; WLSESSION=237134476.20480.0000; TS01b03060=012f350623fd5c8dfef53c8016f92209f692fbb878553da77b0d7cb39209593f4e4b723514009901f6c5b52df817feb4a48b460b42d9df924fe4900f2a2d526564dca63f806c55f1e5df6976d59309bdc6da9da559289a09cdd40962dbf4e8de8db87317cf64aab18502ee0ab644b7f10d146a97046589963400ea55e921974f69ce15ca4f1c31e52480121d523b747f9a1b386f836d70ce861539d87205e46153e534c03bdbbad660826925cd8c3991f7bb8ecf77"
+        #     }
+        # },
         # Virtual Reality (Journal)
         {
             "name": "vr",
@@ -501,24 +511,65 @@ if __name__ == "__main__":
                 'showAll[1]': 'true',
                 'search-within': 'Journal',
                 'facet-journal-id': '10055',
-            }
+            },
+            "postprocessing_paper": [
+                {
+                    "type": "query",
+                    "query": {
+                        "should": [
+                            { 
+                                "must": [
+                                    {
+                                        "should": [
+                                            {"match": { "title": "select*" }},
+                                            {"match": { "title": "manipulat*" }}
+                                        ]
+                                    },
+                                    {
+                                        "should": [
+                                            {"match": { "title": "virtual" }},
+                                            {"match": { "title": "VR" }}
+                                        ]
+                                    }
+                                ]
+                            },
+                            { 
+                                "must": [
+                                    {
+                                        "should": [
+                                            {"match": { "abstract": "select*" }},
+                                            {"match": { "abstract": "manipulat*" }}
+                                        ]
+                                    },
+                                    {
+                                        "should": [
+                                            {"match": { "abstract": "virtual" }},
+                                            {"match": { "abstract": "VR" }}
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
         },
-        # VRST
-        {
-            "name": "vrst",
-            "config_name": "acm",
-            "search_parameters": {
-                'fillQuickSearch': 'false',
-                'ContentItemType': 'research-article',
-                'SpecifiedLevelConceptID': '119205',
-                'expand': 'dl',
-                'AfterMonth': '1',
-                'AfterYear': '2000',
-                'BeforeMonth': '12',
-                'BeforeYear': '2019',
-                'AllField': 'Title:((select* OR manipulat*) AND ("virtual" OR "VR")) OR Abstract:((select* OR manipulat*) AND ("virtual" OR "VR"))'
-            }
-        },
+        # # VRST
+        # {
+        #     "name": "vrst",
+        #     "config_name": "acm",
+        #     "search_parameters": {
+        #         'fillQuickSearch': 'false',
+        #         'ContentItemType': 'research-article',
+        #         'SpecifiedLevelConceptID': '119205',
+        #         'expand': 'dl',
+        #         'AfterMonth': '1',
+        #         'AfterYear': '2000',
+        #         'BeforeMonth': '12',
+        #         'BeforeYear': '2019',
+        #         'AllField': 'Title:((select* OR manipulat*) AND ("virtual" OR "VR")) OR Abstract:((select* OR manipulat*) AND ("virtual" OR "VR"))'
+        #     }
+        # },
         # # Journal of Human-Computer Interaction
         # {
         #     "name": "hci",
@@ -576,9 +627,9 @@ if __name__ == "__main__":
     for finder in finders:
         find = Fetcher(restrict_identifiers_to=restrict, **finder)
         find.run()
-        # find.export_results_to_csv(['title', 'authors', 'doi', 'identifier', 'pdf_url'], file_path=fp)
-        # find.export_results_to_csv(['identifier'], file_path='./ids.csv')
-        # find.download_pdfs()
+        # find.export_results_to_csv(['pdf_file_name', 'title', 'authors', 'doi_url'], file_path=fp)
+        # find.export_results_to_csv(['title', 'pdf_url'])
+        # find.download_pdfs(save_folder='./pdfs/codingExercise')
         # find.export_results_to_csv(['pdf_num_pages'], file_path=fp2, defaults={'pdf_num_pages': 0})
     toc = time.perf_counter()
     h.console_down()
